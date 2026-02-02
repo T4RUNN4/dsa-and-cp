8e38db03411ce4089b5bc34be8f5ee28b087821e
Tarunna <tarunnasen@gmail.com>
2026-02-02 11:51:23 +0600

    Factorial

---
+++ mathematics/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int result = 1;
+    for(int i=2; i<=n; i++) {
+        result*=i;
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    cout << "Factorial of " << n << " is " << factorial(n);
+    return 0;
+}
