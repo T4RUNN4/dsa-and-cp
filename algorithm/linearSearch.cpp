1bd1cbf3e7d581f1a1a08bdb814b79da3c972bde
Tarunna <tarunnasen@gmail.com>
2026-02-03 21:43:36 +0600

    Modified linear search

--- algorithm/linearSearch.cpp
+++ algorithm/linearSearch.cpp
@@ -2,21 +2,18 @@
 using namespace std;
 
 int linearSearch(int arr[], int target, int size) {
-    int index = -1;
-
     for(int i=0; i<size; i++) {
         if(arr[i] == target) {
-            index = i;
-            break;
+            return i;
         }
     }
 
-    return index;
+    return -1;
 }
 
 int main() {
     int size = 6;
-    int arr[size] = {4, 7, 6, 3, 2};
+    int arr[size] = {4, 8, 6, 3, 2};
 
     int result = linearSearch(arr, 8, size);
     (result >= 0) ? cout << "The number is at: " << result << "\n" : cout << "The number is not in array";
