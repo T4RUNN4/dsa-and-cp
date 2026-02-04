e2ef4516500bec11721e16e24dcf07e998299f03
Tarunna <tarunnasen@gmail.com>
2026-02-04 18:04:31 +0600

    Vector included in linear search

--- algorithm/linearSearch.cpp
+++ algorithm/linearSearch.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// array approach
 int linearSearch(int arr[], int target, int size) {
     for(int i=0; i<size; i++) {
         if(arr[i] == target) {
@@ -11,10 +13,29 @@ int linearSearch(int arr[], int target, int size) {
     return -1;
 }
 
-int main() {
-    int size = 6;
-    int arr[size] = {4, 8, 6, 3, 2};
+// vector approach
+int linearSearchVector(vector<int> vect, int target) {
+    for(int i=0; i<vect.size(); i++) {
+        if(vect[i] == target) {
+            return i;
+        }
+    }
 
-    int result = linearSearch(arr, 8, size);
+    return -1;
+}
+
+int main() {
+    
+    // array approach tester
+
+    // int size = 6;
+    // int arr[size] = {4, 8, 6, 3, 2};
+    // int result = linearSearch(arr, 8, size);
+    // (result >= 0) ? cout << "The number is at: " << result << "\n" : cout << "The number is not in array";
+
+
+    //vector approach tester
+    vector<int> vect = {4, 8, 6, 3, 2};
+    int result = linearSearchVector(vect, 8);
     (result >= 0) ? cout << "The number is at: " << result << "\n" : cout << "The number is not in array";
 }
