a8c3af5c82d495d718c327e9a110cb1d06601be9
Tarunna <tarunnasen@gmail.com>
2026-02-04 11:30:45 +0600

    Array Intersection

---
+++ misc/intersection.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr1[5] = {1, 2, 3, 4, 5};
+    int arr2[2] = {3, 4};
+
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<2; j++) {
+            if(arr1[i] == arr2[j]) {
+                cout << arr1[i] << " ";
+            }
+        }
+    }
+    cout << "\n";
+}
