76be70b62c539db26d3dff055a1d825c621945ef
Tarunna <tarunnasen@gmail.com>
2026-02-04 17:53:58 +0600

    Leet - Single Number (136)

---
+++ leetcode/singlenumber.cpp
@@ -0,0 +1,20 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+//Logic: Since x^x = 0 and x^0 = x; duplicate number will turn into 0 by cancelling out each other and unique number will remain xoring with 0
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector <int> vec = {4, 1, 2, 1, 2};
+    int ans = 0;
+
+    for(int val : vec) {
+        ans ^= val;
+    }
+
+    cout << ans << "\n";
+}
