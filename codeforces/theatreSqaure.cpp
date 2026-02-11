ea5055baa217e853af797e6d57fdda51eab5cfe5
Tarunna <tarunnasen@gmail.com>
2026-02-11 17:52:27 +0600

    Theatre Square Question included

--- codeforces/theatreSqaure.cpp
+++ codeforces/theatreSqaure.cpp
@@ -12,3 +12,8 @@ int main() {
     cout << h*w << "\n";
     return 0;
 }
+
+// Qus: Theatre Square in the capital city of Berland has a rectangular shape with the size n × m meters. On the occasion of the city's anniversary, a decision was taken to pave the Square with square granite flagstones. Each flagstone is of the size a × a. What is the least number of flagstones needed to pave the Square? It's allowed to cover the surface larger than the Theatre Square, but the Square has to be covered. It's not allowed to break the flagstones. The sides of flagstones should be parallel to the sides of the Square.
+
+// Logic
+// Since parallel number of stone is needed and stone can exceed the pave, first it need to be calculated the minimum number of stone needed in height and width. Then the total number of stone can be calculated
